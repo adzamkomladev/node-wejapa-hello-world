dbf5afface4a6ab0ae818499915e167d4ee994cd
adzamkomladev <adzamkomla.dev@gmail.com>
2020-07-25 16:30:40 +0000

    Ref. route handling code into functions

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const http = require('http');
 
+const getHello = (request, response) => {
+    response.statusCode = 200;
+    response.end('Hello World, Welcome to WeJapa Internships');
+};
+
+const postHello = (request, response) => {
+    let body = [];
+
+    request.on('data', chunk => body.push(chunk)).on('end', () => {
+        body = Buffer.concat(body).toString();
+        body = JSON.parse(body);
+
+        response.statusCode = 200;
+        response.end(`Hello ${body.name}, Welcome to WeJapa Internships`);
+    });
+};
+
 const server = http.createServer((request, response) => {
     const { url, method } = request;
 
     if (url === '/' && method === 'GET') {
-        response.statusCode = 200;
-        response.end('Hello World, Welcome to WeJapa Internships');
+        getHello(request, response);
     } else if (method === 'POST' && url === '/') {
-        let body = [];
-
-        request.on('data', chunk => body.push(chunk)).on('end', () => {
-            body = Buffer.concat(body).toString();
-            body = JSON.parse(body);
-
-            response.statusCode = 200;
-            response.end(`Hello ${body.name}, Welcome to WeJapa Internships`);
-        });
+        postHello(request, response);
     } else {
         response.statusCode = 404;
         response.end(`Route '${url}' with method '${method}' does not exist`);
@@ -25,5 +33,3 @@ const server = http.createServer((request, response) => {
 server.listen(3000);
 
 server.on('listening', () => console.log('Server has started'));
-
-
