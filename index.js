b48042766e8bec5b32f895b556090de67391ccd8
adzamkomladev <adzamkomla.dev@gmail.com>
2020-07-25 16:00:15 +0000

    Add / GET route

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((request, response) => {
+    const { url, method } = request;
+
+    if (url === '/' && method === 'GET') {
+        response.statusCode = 200;
+        response.end('Hello World, Welcome to WeJapa Internships');
+    } else {
+        response.statusCode = 404;
+        response.end(`Route '${url}' with method '${method}' does not exist`);
+    }
+});
 
 server.listen(3000);
 
