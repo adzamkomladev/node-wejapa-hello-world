2c15fd64006b4ecd8b1663b7d26a89b1b9f95bdd
adzamkomladev <adzamkomla.dev@gmail.com>
2020-07-25 16:07:04 +0000

    Add / POST route

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const server = http.createServer((request, response) => {
     if (url === '/' && method === 'GET') {
         response.statusCode = 200;
         response.end('Hello World, Welcome to WeJapa Internships');
+    } else if (method === 'POST' && url === '/') {
+        let body = [];
+
+        request.on('data', chunk => body.push(chunk)).on('end', () => {
+            body = Buffer.concat(body).toString();
+            body = JSON.parse(body);
+
+            response.statusCode = 200;
+            response.end(`Hello ${body.name}, Welcome to WeJapa Internships`);
+        });
     } else {
         response.statusCode = 404;
         response.end(`Route '${url}' with method '${method}' does not exist`);
